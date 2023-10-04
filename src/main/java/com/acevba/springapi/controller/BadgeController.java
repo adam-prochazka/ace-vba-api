e08985f773e9ff22105a89ee283e82784061a610
Adam Prochazka <adam.prochazka@icloud.com>
2023-10-04 15:01:33 +0100

    Add ResponseEntity to methods in BadgeController

--- src/main/java/com/acevba/springapi/controller/BadgeController.java
+++ src/main/java/com/acevba/springapi/controller/BadgeController.java
@@ -4,6 +4,8 @@ import com.acevba.springapi.exception.ResourceNotFoundException;
 import com.acevba.springapi.model.Badge;
 import com.acevba.springapi.repository.BadgeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -18,43 +20,45 @@ public class BadgeController {
     private BadgeRepository badgeRepository;
 
     @GetMapping("/badges")
-    public List<Badge> getAllBadges() {
+    public ResponseEntity<List<Badge>> getAllBadges() {
         List<Badge> badges = new ArrayList<>();
         badges.addAll(badgeRepository.findAll());
-        return badges;
+        return new ResponseEntity<>(badges, HttpStatus.OK);
     }
 
     @GetMapping("/badges/{id}")
-    public Badge getBadgeById(@PathVariable Long id) {
-        return badgeRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Badge with id = " + id));
+    public ResponseEntity<Badge> getBadgeById(@PathVariable Long id) {
+        return new ResponseEntity<>(badgeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Badge with id = " + id)), HttpStatus.OK);
     }
 
     @PostMapping("/badges")
-    public Badge createBadge(@RequestBody Badge badge) {
-        return badgeRepository.save(badge);
+    public ResponseEntity<Badge> createBadge(@RequestBody Badge badge) {
+        return new ResponseEntity<>(badgeRepository.save(badge), HttpStatus.CREATED);
     }
 
     /**
      * Preserve id
      */
     @PutMapping("/badges/{id}")
-    Badge updateBadge(@RequestBody Badge badge, @PathVariable Long id) {
+    public ResponseEntity<Badge> updateBadge(@RequestBody Badge badge, @PathVariable Long id) {
         Badge _badge = badgeRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Badge with id = " + id));
         _badge.setLevel(badge.getLevel());
         _badge.setRole(badge.getRole());
         _badge.setUsers(badge.getUsers());
-        return badgeRepository.save(_badge);
+        return new ResponseEntity<>(badgeRepository.save(_badge), HttpStatus.OK);
     }
 
     @DeleteMapping("/badges/{id}")
-    void deleteBadge(@PathVariable Long id) {
+    public ResponseEntity<HttpStatus> deleteBadge(@PathVariable Long id) {
         badgeRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping("/badges")
-    void deleteAllBadges() {
+    public ResponseEntity<HttpStatus> deleteAllBadges() {
         badgeRepository.deleteAll();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
