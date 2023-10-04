5e744d517b54ef5541ff72cafea795faeef70b09
Adam Prochazka <adam.prochazka@icloud.com>
2023-10-04 14:54:03 +0100

    Add ResponseEntity to UserController methods

--- src/main/java/com/acevba/springapi/controller/UserController.java
+++ src/main/java/com/acevba/springapi/controller/UserController.java
@@ -5,6 +5,7 @@ import com.acevba.springapi.repository.UserRepository;
 import com.acevba.springapi.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -20,44 +21,51 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping("/users")
-    public List<User> getAllUsers(@RequestParam Optional<String> username) {
+    public ResponseEntity<List<User>> getAllUsers(@RequestParam Optional<String> username) {
         List<User> users = new ArrayList<>();
 
         if (username.isPresent())
             users.addAll(userRepository.findByUsername(username.get()));
         else
             users.addAll(userRepository.findAll());
-        return users;
+
+        if (users.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     @GetMapping("/users/{id}")
-    public User getUserById(@PathVariable Long id) {
-        return userRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("User with id = " + id));
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return new ResponseEntity<>(userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User with id = " + id)), HttpStatus.OK);
     }
 
     @PostMapping("/users")
-    public User createUser(@RequestBody User user) {
-        return userRepository.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return new ResponseEntity<>(userRepository.save(user), HttpStatus.CREATED);
     }
 
     @PutMapping("/users/{id}")
-    User updateUser(@RequestBody User user, @PathVariable Long id) {
+    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable Long id) {
         User _user = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User with id = " + id));
         _user.setUsername(user.getUsername());
         _user.setBadges(user.getBadges());
         _user.setEvents(user.getEvents());
-        return userRepository.save(_user);
+        return new ResponseEntity<>(userRepository.save(_user), HttpStatus.OK);
     }
 
     @DeleteMapping("/users/{id}")
-    void deleteUser(@PathVariable Long id) {
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable Long id) {
         userRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping("/users")
-    void deleteAllUsers() {
+    public ResponseEntity<HttpStatus> deleteAllUsers() {
         userRepository.deleteAll();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
