781cb4b5ea3be694f37deb7457936db7ea421c28
adam-prochazka <adam.prochazka@icloud.com>
2023-10-12 22:00:34 +0100

    Add swagger annotations to exception controller

--- src/main/java/com/acevba/springapi/exception/ControllerExceptionHandler.java
+++ src/main/java/com/acevba/springapi/exception/ControllerExceptionHandler.java
@@ -2,6 +2,10 @@ package com.acevba.springapi.exception;
 
 import java.util.Date;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -9,29 +13,32 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
+@Tag(name = "Error Handling"/*, description = "Controller for handling errors."*/)
 public class ControllerExceptionHandler {
 
+    @Operation(summary = "Handle ResourceNotFoundException", tags = { "Error Handling" }, responses = {
+            @ApiResponse(responseCode = "404", description = "Resource not found.")
+    })
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public ErrorMessage resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-        ErrorMessage message = new ErrorMessage(
+        return new ErrorMessage(
                 HttpStatus.NOT_FOUND.value(),
                 new Date(),
                 "[NOT-FOUND]: " + ex.getMessage(),
                 request.getDescription(false));
-
-        return message;
     }
 
+    @Operation(summary = "Handle Exception", tags = { "Error Handling" }, responses = {
+            @ApiResponse(responseCode = "500", description = "Internal server error.")
+    })
     @ExceptionHandler(Exception.class)
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorMessage globalExceptionHandler(Exception ex, WebRequest request) {
-        ErrorMessage message = new ErrorMessage(
+        return new ErrorMessage(
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 new Date(),
                 "[ERROR]: " + ex.getMessage(),
                 request.getDescription(false));
-
-        return message;
     }
 }
