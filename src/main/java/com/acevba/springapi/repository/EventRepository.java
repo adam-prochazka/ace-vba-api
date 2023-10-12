cbc85cc95ab04f186bcccf6208a509b3eec036fb
adam-prochazka <adam.prochazka@icloud.com>
2023-10-12 22:04:38 +0100

    Add event repository

---
+++ src/main/java/com/acevba/springapi/repository/EventRepository.java
@@ -0,0 +1,7 @@
+package com.acevba.springapi.repository;
+
+import com.acevba.springapi.model.Badge;
+import com.acevba.springapi.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Long> {}
