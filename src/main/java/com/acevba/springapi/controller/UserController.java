a971da357f30e40fef1208bb24f11b545b933a35
adam-prochazka <adam.prochazka@icloud.com>
2023-10-12 21:55:01 +0100

    Add swagger annotations to User controller

--- src/main/java/com/acevba/springapi/controller/UserController.java
+++ src/main/java/com/acevba/springapi/controller/UserController.java
@@ -3,11 +3,16 @@ package com.acevba.springapi.controller;
 import com.acevba.springapi.exception.ResourceNotFoundException;
 import com.acevba.springapi.repository.UserRepository;
 import com.acevba.springapi.model.User;
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,11 +20,19 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "User", description = "User management APIs")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Operation(summary = "Get all users", tags = { "users", "get" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = User.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "204", description = "There are no users", content = {
+                    @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @GetMapping("/users")
     public ResponseEntity<List<User>> getAllUsers(@RequestParam Optional<String> username) {
         List<User> users = new ArrayList<>();
@@ -36,17 +49,34 @@ public class UserController {
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
+    @Operation(summary = "Get a user by ID", tags = { "users", "get" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = User.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @GetMapping("/users/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
         return new ResponseEntity<>(userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User with id = " + id)), HttpStatus.OK);
     }
 
+    @Operation(summary = "Create a new user", tags = { "users", "post" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", content = {
+                    @Content(schema = @Schema(implementation = User.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @PostMapping("/users")
     public ResponseEntity<User> createUser(@RequestBody User user) {
         return new ResponseEntity<>(userRepository.save(user), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Update a user", tags = { "users", "put" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = User.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @PutMapping("/users/{id}")
     public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable Long id) {
         User _user = userRepository.findById(id)
@@ -57,12 +87,21 @@ public class UserController {
         return new ResponseEntity<>(userRepository.save(_user), HttpStatus.OK);
     }
 
+
+    @Operation(summary = "Delete a user", tags = { "users", "delete" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @DeleteMapping("/users/{id}")
     public ResponseEntity<HttpStatus> deleteUser(@PathVariable Long id) {
         userRepository.deleteById(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @Operation(summary = "Delete all users", tags = { "users", "delete" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @DeleteMapping("/users")
     public ResponseEntity<HttpStatus> deleteAllUsers() {
         userRepository.deleteAll();
