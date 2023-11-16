744c7571ddb973c3eebfa5848fbf782b8c8f309c
adam-prochazka <adam.prochazka@icloud.com>
2023-11-16 12:13:22 +0000

    Remove Swagger annotations from exception handler

--- src/main/java/com/acevba/springapi/exception/ControllerExceptionHandler.java
+++ src/main/java/com/acevba/springapi/exception/ControllerExceptionHandler.java
@@ -2,23 +2,17 @@ package com.acevba.springapi.exception;
 
 import java.util.Date;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+@Hidden
 @RestControllerAdvice
-@Tag(name = "Error Handling"/*, description = "Controller for handling errors."*/)
 public class ControllerExceptionHandler {
 
-    @Operation(summary = "Handle ResourceNotFoundException", tags = { "Error Handling" }, responses = {
-            @ApiResponse(responseCode = "404", description = "Resource not found.")
-    })
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public ErrorMessage resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
@@ -26,19 +20,16 @@ public class ControllerExceptionHandler {
                 HttpStatus.NOT_FOUND.value(),
                 new Date(),
                 "[NOT-FOUND]: " + ex.getMessage(),
-                request.getDescription(false));
+                request.getDescription(true));
     }
 
-    @Operation(summary = "Handle Exception", tags = { "Error Handling" }, responses = {
-            @ApiResponse(responseCode = "500", description = "Internal server error.")
-    })
     @ExceptionHandler(Exception.class)
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorMessage globalExceptionHandler(Exception ex, WebRequest request) {
         return new ErrorMessage(
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 new Date(),
-                "[ERROR]: " + ex.getMessage(),
+                "[INTERNAL-SERVER-ERROR]: " + ex.getMessage(),
                 request.getDescription(false));
     }
 }
